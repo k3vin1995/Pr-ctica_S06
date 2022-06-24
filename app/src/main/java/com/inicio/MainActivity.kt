8a76290402813919219047366ee8690b158eb791
kevin fallas alfaro <kfallas40817@ufide.ac.cr>
2022-06-23 19:31:56 -0600

    commit 2

--- app/src/main/java/com/inicio/MainActivity.kt
+++ app/src/main/java/com/inicio/MainActivity.kt
@@ -1,11 +1,89 @@
 package com.inicio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.inicio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
         setContentView(R.layout.activity_main)
+
+        binding.btRegister.setOnClickListener { haceRegistro() }
+        binding.btLogin.setOnClickListener { haceLogin() }
+
     }
+
+    private fun haceLogin() {
+        Log.i("haceLogin","entrando")
+        val email = binding.etCorreo.text.toString()
+        val clave = binding.etClave.text.toString()
+        Log.i("haceLogin","antes de hacer registro")
+        //Se usa la función para crear un usuario por medio de correo y contraseña
+        auth.signInWithEmailAndPassword(email,clave)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    actualiza(user)
+                } else {
+                    Toast.makeText(baseContext,
+                        getString(R.string.msg_fallo_login),
+                        Toast.LENGTH_SHORT).show()
+                    actualiza(null)
+                }
+            }
+        Log.i("haceLogin","Fuera de autenticacion")
+    }
+
+    private fun actualiza(user: FirebaseUser?) {
+        if (user!=null) {
+            // paso a la pantalla principal
+            //val intent = Intent(this,Principal::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun haceRegistro() {
+        val email = binding.etCorreo.text.toString()
+        val clave = binding.etClave.text.toString()
+
+        //Se usa la función para crear un usuario por medio de correo y contraseña
+        auth.createUserWithEmailAndPassword(email,clave)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    actualiza(user)
+                } else {
+                    Toast.makeText(baseContext,
+                        getString(R.string.msg_fallo_registro),
+                        Toast.LENGTH_SHORT).show()
+                    actualiza(null)
+                }
+            }
+    }
+
+    //si el usuario se encuentra autenticado, pasa a la siguente pantalla
+    public override fun onStart(){
+        super.onStart()
+        // se obtiene el usuario actual
+        val usuario = auth.currentUser
+        actualiza(usuario)
+    }
+
 }
